c74259a6512db4e67ff171e695df7d77d4cdf340
진석 <ozjinseok@gmail.com>
2025-11-20 13:49:42 +0900

    테일윈드 연습

---
+++ 20251119/tailwind-css/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="text-amber-500">안녕하세요</div>
+    </div>
+  );
+}
+
+export default App;
